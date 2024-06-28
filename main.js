92a1ac4064233acc1f93d6541f07b9bfd5b3f94a
YasminDaniele <127122204+YasminDaniele@users.noreply.github.com>
2024-06-28 11:49:38 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -7,3 +7,14 @@ for(let j=0;j<botoes.length;j++) { botoes[j].classList.remove(ativo);
 botoes[i].classList.add(ativo);
 }
 }
+const textos = document.querySelectorAll(aba-conteudo);
+textos[i].classList.add(ativo);
+textos[j].classList.remove(ativo);
+const contadores = document.querySelectorAll(contador);
+const contadores = document.querySelectorAll(contador);
+contadores[0].textContent = "Contagem regressiva";
+
+const contadores = document.querySelectorAll(contador);
+const tempoObjetivo1 = new Date(2024-10-05T00:00:00);
+let tempoAtual = new Date();
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
